2539ee7a7661aebc2462ad2877ab553b90d559e3
clade <pierre.clade@spectro.jussieu.fr>
2013-04-24 11:21:48 +0200

    Release of version 1.2.5

--- PyDAQmx/__init__.py
+++ PyDAQmx/__init__.py
@@ -6,7 +6,7 @@ from DAQmxFunctions import *
 from Task import Task
 
 
-__version_info__ = (1, 2, 4)
+__version_info__ = (1, 2, 5)
 __version__ = '.'.join(str(num) for num in __version_info__)
 
 __author__ =u'Pierre Cladé'
