b2c274f745947fbd1466ac54edf6f143b1af7e5c
clade <pierre.clade@spectro.jussieu.fr>
2016-07-20 11:08:31 +0200

    Modify the init of PyDAQmx to rename function without prefix

--- PyDAQmx/__init__.py
+++ PyDAQmx/__init__.py
@@ -7,6 +7,18 @@ from DAQmxConstants import *
 from DAQmxFunctions import *
 from Task import Task
 
+import DAQmxConstants
+import DAQmxFunctions
+
+__all__ = DAQmxConstants.constant_list + DAQmxFunctions.function_dict.keys() + ['Task']
+
+for name in DAQmxConstants.constant_list + DAQmxFunctions.function_dict.keys():
+    if name.startswith('DAQmx_'):
+        new_name = name[6:]
+    elif name.startswith('DAQmx'):
+        new_name = name[5:]
+    globals()[new_name] = globals()[name]
+
 
 __version_info__ = (1, 3)
 __version__ = '.'.join(str(num) for num in __version_info__)
