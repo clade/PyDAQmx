f1f03827a8c40b3cd9455d98241c14e43d44fd77
Pierre Cladé <pierre.clade@spectro.jussieu.fr>
2016-09-05 19:40:49 +0200

    Add __main__ to the repo

---
+++ __main__.py
@@ -0,0 +1,63 @@
+import unittest
+import argparse
+
+""" Main file use to test the library
+
+For example ::
+
+    python __main__.py 
+    python __main__.py legacy
+
+to test the new or legacy version. 
+
+It is also possible to zip the lib obtained with ``python setup.py build``, including this file and run ::
+
+    python lib.zip 
+
+This is usefull to test on a remote system. 
+
+"""
+
+parser = argparse.ArgumentParser(description='Test the PyDAQmx library')
+
+parser.add_argument('version', choices=['pep8', 'legacy'], default='pep8', nargs="?")
+
+args = parser.parse_args()
+
+version = args.version
+
+if version=='pep8':
+    try:  
+        import pydaqmx
+    except NotImplementedError:
+        import daqmxconfigtest
+
+    import pydaqmx
+    import pydaqmx.examples
+    import pydaqmx.test
+
+    print("Functions and constants are imported from : " + pydaqmx.config.dot_h_file)
+
+    if pydaqmx.config.lib_name is None:
+        print('DAQmx is not installed. pydaqmx is using a dummy library for tests')
+    #            unittest.main('PyDAQmxTest', "suite_base", [unittest.__file__])
+        unittest.main('pydaqmx.test', "suite_base", [unittest.__file__])
+    else:
+        print("The library is : " + pydaqmx.config.lib_name)
+        unittest.main('pydaqmx.test', "alltests", [unittest.__file__])    
+else:
+    try:  
+        import PyDAQmx
+    except NotImplementedError:
+        import daqmxconfigtest
+
+    import PyDAQmx
+
+    print("Functions and constants are imported from : " + PyDAQmx.DAQmxConfig.dot_h_file)
+
+    if PyDAQmx.DAQmxConfig.lib_name is None:
+        print('DAQmx is not installed. PyDAQmx is using a dummy library for tests')
+        unittest.main('PyDAQmx.PyDAQmxTest', "suite_base", [unittest.__file__])
+    else:
+        print("The library is : " + PyDAQmx.DAQmxConfig.lib_name)
+        unittest.main('PyDAQmx.PyDAQmxTest', "alltests", [unittest.__file__])  
